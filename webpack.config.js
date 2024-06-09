4ba6c4af09f4bc30a549a9bcafbfff88263ba248
Jamie Peabody <jamie.peabody@gmail.com>
2024-06-09 22:30:19 +0100

    feat: react and react-dom are now peerDependencies and not bundled

--- webpack.config.js
+++ webpack.config.js
@@ -35,12 +35,22 @@ module.exports = function(env, argv) {
 
 		output: {
 			filename: 'mergely.js',
-			path: path.resolve(__dirname, 'lib')
+			path: path.resolve(__dirname, 'lib'),
+			library: {
+				name: 'mergely-react',
+				type: 'umd',
+			}
+		},
+
+		externals: {
+			react: 'react',
+			'react-dom' : 'reactDOM'
 		}
 	};
 	if (!production) {
 		config.entry = './src/index.jsx';
 		config.devtool = 'eval';
+		delete config.externals;
 		config.plugins.push(
 			new HtmlWebpackPlugin({
 				templateContent: '<div id="root"></div>'
